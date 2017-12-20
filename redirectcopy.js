397efd1fa1951f8d7936e7d05556821c71a05519
nguyenvantrong1997 <34577136+nguyenvantrong1997@users.noreply.github.com>
2017-12-20 21:49:31 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://vietthanhmusic.vn/'>https://vietthanhmusic.vn/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://thoitrangphuyen.com/'>http://thoitrangphuyen.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
